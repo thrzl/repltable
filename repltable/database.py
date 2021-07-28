3901383c78a905860e0788ca51e156b0369206a3
Mikey <73202594+terabyte3@users.noreply.github.com>
2021-07-28 11:15:12 -0400

    📝 add docstrings, remove unneeded import

--- repltable/database.py
+++ repltable/database.py
@@ -1,11 +1,13 @@
 from typing import List
 from replit import Database as rdb
-from replit import db
 
 
 class Database:
     def __init__(self, database: rdb):
         self._db = database
+
+    def __setitem__(self, name: str, value: str) -> None:
+        self._db[name] = value
 
     def __getitem__(self, key: str):
         if key not in self._db.keys():
@@ -17,11 +19,22 @@ class Database:
             del self._db[name]
 
     def drop(self, table: str):
+        """Drops a table from the database.
+
+        Parameters
+        ----------
+        table : str
+            The name of the table to drop.
+        """        
         if table in self._db.keys():
             del self._db[table]
 
 
 class Table:
+    """An object representing a table in the database.
+
+    You should not need to create an instance of this class yourself.
+    """    
     def __init__(self, docs: List[dict], db: Database, name: str):
         self._documents = docs
         self.db = db
@@ -31,6 +44,13 @@ class Table:
         self.db[self.name] = self._documents
 
     def get(self, **kwargs):
+        """Gets all documents matching the given query.
+
+        Returns
+        -------
+        list[dict]
+            Returns a list of documents matching the given query.
+        """        
         return [
             i
             for i in self._documents
@@ -38,13 +58,36 @@ class Table:
         ]
 
     def get_one(self, **kwargs):
+        """Gets the first document matching the given query.
+
+        Returns
+        -------
+        dict
+            The document found.
+        """        
         return self.get(**kwargs)[0]
 
     def insert(self, data: dict):
+        """Insert a new document into the table.
+
+        Parameters
+        ----------
+        data : dict
+            A dictionary containing the data to insert.
+        """        
         self._documents.append(data)
         self.__update_changes()
 
     def update(self, data: dict, **kwargs):
+        """Update an existing document in the table.
+
+        Parameters
+        ----------
+        data : dict
+            The new data
+        **filters
+            Filters that the document must match.
+        """        
         for index, doc in enumerate(self._documents):
             for query, ans in kwargs.items():
                 if doc[query] == ans:
@@ -52,4 +95,6 @@ class Table:
         self.__update_changes()
 
     def drop(self):
+        """Delete this table.
+        """        
         delattr(self.db, self.name)
