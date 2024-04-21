38014d487e4631c98c0254563098ff8a7246f3ed
te <thrzl@icloud.com>
2024-04-20 21:19:57 -0400

    ✅ improve benchmarks

--- bench_replit.py
+++ bench_replit.py
@@ -1,7 +1,12 @@
 from replit import db
 from timeit import timeit
 
+db.set("test", "item")
+
 def bench_replit():
-    db.get("amog")
+    db.get("test")
+
 
 print(timeit(bench_replit, number=10))
+
+del db["test"]
--- bench_repltable.py
+++ bench_repltable.py
@@ -1,7 +1,12 @@
 from timeit import timeit
 from repltable import db
 
+db.set("test", "item")
+
 def bench_repltable():
-    db.get("amog")
+    db.get("test")
+
 
 print(timeit(bench_repltable, number=10))
+
+db.delete("test")
