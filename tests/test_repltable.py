17c42bfcbd8fe07e4c30b9ffd09883b46d732303
te <thrzl@icloud.com>
2024-04-21 14:26:31 -0400

    ✅ add/fix tests

--- tests/test_repltable.py
+++ tests/test_repltable.py
@@ -1,4 +1,4 @@
-from repltable import TableDatabase, Table  # type: ignore
+from repltable import Database, Table  # type: ignore
 from dotenv import load_dotenv
 from os import environ
 import pytest
@@ -8,35 +8,46 @@ load_dotenv(".env.local")
 if not environ.get("REPLIT_DB_URL"):
     pytest.exit("REPLIT_DB_URL not found.")
 
-db = TableDatabase(db_url=environ["REPLIT_DB_URL"])
+db = Database(db_url=environ["REPLIT_DB_URL"])
 
 
 def test_drop_all_iter():
-    for i in db:
-        db.drop(i)
-    assert db.tables() == []
+    for i in db.list_keys():
+        db.delete(i)
+    assert db.list_keys() == []
 
 
 def test_get_table():
-    table = db["newtesttable"]
+    table = db.get_table("newtesttable")
     print(table)
     assert isinstance(table, Table)
 
 
 def test_get_key():
-    table = db["newtesttable"]
+    table = db.get_table("newtesttable")
     print(table.get(fdsfds=1))
     assert not table.get(id=231231)
 
 
 def test_drop_table():
-    table = db["newtesttable"]
-    table.drop()
-    assert getattr(db, "newtesttable", None) is None
+    db.delete("newtesttable")
+    assert db.get("newtesttable") is None
+
+
+def test_db_set_get():
+    db.set("test", "item")
+    assert db.get("test") == "item"
+    db.delete("test")
+    assert db.get("test") is None
+    db.set_bulk({"test": "item", "test2": "item2"})
+    assert db.get("test") == "item"
+    assert db.get("test2") == "item2"
+    db.delete("test")
+    db.delete("test2")
 
 
 def test_set_get():
-    table = db["things"]
+    table = db.get_table("things")
     table.insert(dict(name="test", value=1, id=2131))
     assert isinstance(table.get(id=2131), list) and (table.get(id=2131))[0]["name"] in [
         "test",
@@ -56,15 +67,15 @@ def test_set_get():
 
 
 def test_drop_table_with_data():
-    table = db["things"]
+    table = db.get_table("things")
     table.insert(dict(name="test", value=1, id=2131))
     table.insert(dict(name="test2", value=2, id=2132))
-    table.drop()
+    db.delete("things")
     assert getattr(db, "things", None) is None
 
 
 def test_delete():
-    table = db["things"]
+    table = db.get_table("things")
     table.insert(dict(name="test", value=1, id=2131))
     table.insert(dict(name="test2", value=2, id=2132))
     table.delete(id=2131)
@@ -72,5 +83,5 @@ def test_delete():
 
 
 def drop_table_from_db():
-    db.drop("things")
+    db.delete("things")
     assert getattr(db, "things", None) is None
