841fa30bb3c85762d5b20e621aeb4a4180f5c7d8
te <thrzl@icloud.com>
2024-04-21 20:47:13 -0400

    ➕ use pytest-asyncio + change version

--- tests/test_repltable_async.py
+++ tests/test_repltable_async.py
@@ -12,32 +12,33 @@ if not environ.get("REPLIT_DB_URL"):
 @pytest.fixture(scope="function")
 def db():
     return Database(db_url=environ["REPLIT_DB_URL"])
+# db = Database(db_url=environ["REPLIT_DB_URL"])
 
-
-async def test_drop_all_iter(db):
+@pytest.mark.asyncio
+async def test_drop_all_iter():
     for i in await db.keys():
         await db.delete(i)
     assert await db.keys() == []
 
-
-async def test_get_table(db):
+@pytest.mark.asyncio
+async def test_get_table():
     table = await db.get_table("newtesttable")
     print(table)
     assert isinstance(table, Table)
 
-
-async def test_get_key(db):
+@pytest.mark.asyncio
+async def test_get_key():
     table = await db.get_table("newtesttable")
     print(await table.get(fdsfds=1))
     assert not await table.get(id=231231)
 
-
-async def test_drop_table(db):
+@pytest.mark.asyncio
+async def test_drop_table():
     await db.delete("newtesttable")
     assert await db.get("newtesttable") is None
 
-
-async def test_db_set_get(db):
+@pytest.mark.asyncio
+async def test_db_set_get():
     await db.set("test", "item")
     assert await db.get("test") == "item"
     await db.delete("test")
@@ -48,8 +49,8 @@ async def test_db_set_get(db):
     await db.delete("test")
     await db.delete("test2")
 
-
-async def test_set_get(db):
+@pytest.mark.asyncio
+async def test_set_get():
     table = await db.get_table("things")
     await table.insert(dict(name="test", value=1, id=2131))
     assert isinstance(await table.get(id=2131), list) and (await table.get(id=2131))[0][
@@ -75,23 +76,23 @@ async def test_set_get(db):
         and test_item["flob"] == "baz"
     )
 
-
-async def test_drop_table_with_data(db):
+@pytest.mark.asyncio
+async def test_drop_table_with_data():
     table = await db.get_table("things")
     await table.insert(dict(name="test", value=1, id=2131))
     await table.insert(dict(name="test2", value=2, id=2132))
     await db.delete("things")
     assert getattr(db, "things", None) is None
 
-
-async def test_delete(db):
+@pytest.mark.asyncio
+async def test_delete():
     table = await db.get_table("things")
     await table.insert(dict(name="test", value=1, id=2131))
     await table.insert(dict(name="test2", value=2, id=2132))
     await table.delete(id=2131)
     assert not await table.get(id=2131)
 
-
-async def drop_table_from_db(db):
+@pytest.mark.asyncio
+async def drop_table_from_db():
     await db.delete("things")
     assert getattr(db, "things", None) is None
